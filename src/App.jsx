5e8ebbb86110adefbad7795ae81487a8f170fca5
Sri Harsha <sriharshabhoomandla@gmail.com>
2025-08-31 19:54:22 +0530

    Updated error

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="*" element={<Notfound />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
