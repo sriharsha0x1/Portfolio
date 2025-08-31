2bc568d30b0aeee1cb9c99e3ad77e4cde7467f10
Sri Harsha <sriharshabhoomandla@gmail.com>
2025-08-31 19:57:22 +0530

    error updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
+//import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
+          
         </Routes>
       </BrowserRouter>
     </>
